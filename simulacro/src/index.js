eda34238bc8bf0f0803d9e6e774fdd93cb6edc65
afran012 <afran.012@gmail.com>
2022-01-25 13:55:28 -0500

    add get delete

--- simulacro/src/index.js
+++ simulacro/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {AppRouter} from './routers/AppRouter';
+import './styles/css/body.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppRouter />,
   document.getElementById('root')
 );
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Alexandra Cortés	C.C	1234569785	4569877	3158965478	cr 78 cll 90 88
+
